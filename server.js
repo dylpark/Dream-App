3fbb43ff74e4e4d7036dbc1f0d044c500a5b1726
Dylan Park <64296934+dylpark@users.noreply.github.com>
2023-02-05 23:41:09 +1000

    Update server.js

--- server.js
+++ server.js
@@ -1,32 +1,32 @@
-import * as dotenv from 'dotenv';
+import * as dotenv from "dotenv";
 dotenv.config();
 
-import { Configuration, OpenAIApi } from 'openai';
+import { Configuration, OpenAIApi } from "openai";
 
 const configuration = new Configuration({
     apiKey: process.env.OPENAI,
-})
+});
 
 const openAi = new OpenAIApi(configuration);
 
-import express from 'express';
-import cors from 'cors';
+import express from "express";
+import cors from "cors";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.post('/dream', async (req, res) => {
+app.post("/dream", async (req, res) => {
     const prompt = req.body.prompt;
 
-    const aiRepsonse = await openAi.createImage({
+    const aiResponse = await openAi.createImage({
         prompt,
         n: 1,
-        size: '1024x1024',
+        size: "1024x1024",
     });
 
     const image = aiResponse.data.data[0].url;
     res.send({ image });
 });
 
-app.listen(8080, () => console.log('Make art on http://localhost:8080/dream'));
+app.listen(8080, () => console.log("Make art on http://localhost:8080/dream"));
